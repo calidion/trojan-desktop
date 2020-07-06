0e9d466d9ca6eadd1306f61b8318bba004027508
calidion <calidion@gmail.com>
2020-07-06 23:46:52 +0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ElectronService } from './core/services';
+import { TranslateService } from '@ngx-translate/core';
+import { AppConfig } from '../environments/environment';
+
+import { Title } from "@angular/platform-browser";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private electronService: ElectronService,
+    private translate: TranslateService,
+    private title: Title
+  ) {
+    this.translate.setDefaultLang('en');
+    this.translate.setDefaultLang('zh');
+    console.log('AppConfig', AppConfig);
+    this.translate.get("TITLE").subscribe((title) => {
+      this.title.setTitle(title);
+    });
+
+    if (electronService.isElectron) {
+      console.log(process.env);
+      console.log('Run in electron');
+      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
+      console.log('NodeJS childProcess', this.electronService.childProcess);
+    } else {
+      console.log('Run in browser');
+    }
+  }
+}
