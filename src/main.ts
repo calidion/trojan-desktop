299a862746c94908c0ab326373d3a89a5d2f983c
calidion <calidion@gmail.com>
2020-07-07 02:23:29 +0800

    fixed a bug on importing bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { AppConfig } from './environments/environment';
 
+import "jquery";
+import "bootstrap";
+
+declare var jQuery: any;
+declare var $: any;
+
 if (AppConfig.production) {
   enableProdMode();
 }
