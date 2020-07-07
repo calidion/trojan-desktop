334ea141685b188e055fd775050d256a6b7ec51a
calidion <calidion@gmail.com>
2020-07-07 15:09:37 +0800

    init input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+const config = require("../../assets/config.json");
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,7 +10,9 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    console.log(config);
+   }
 
   ngOnInit(): void { }
 
