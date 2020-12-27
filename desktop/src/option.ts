61a33af5d6ae7b9e0ca8801c9777850ed7d85648
calidion <calidion@gmail.com>
2020-12-27 19:33:15 +0800

    added setting icon

--- desktop/src/option.ts
+++ desktop/src/option.ts
@@ -2,8 +2,15 @@ import * as path from "path";
 
 export default {
   icon: {
-    file: path.resolve(path.join(__dirname, "../../frontend/assets/icons/favicon.png")),
-    exit: path.resolve(path.join(__dirname, "../../frontend/assets/icons/exit.png")),
+    file: path.resolve(
+      path.join(__dirname, "../../frontend/assets/icons/favicon.png")
+    ),
+    exit: path.resolve(
+      path.join(__dirname, "../../frontend/assets/icons/exit.png")
+    ),
+    settings: path.resolve(
+      path.join(__dirname, "../../frontend/assets/icons/settings.png")
+    ),
   },
   local: {
     host: "http://localhost:4200",
--- desktop/src/tray.ts
+++ desktop/src/tray.ts
@@ -5,9 +5,9 @@ let tray: Tray = null;
 export function initTray(options, app) {
   tray = new Tray(options.icon.file);
   const contextMenu = Menu.buildFromTemplate([
-    { label: "Disconnect", type: "radio", checked: true },
-    { label: "Connect", type: "radio" },
-    { label: "Settings", type: "radio" },
+    // { label: "Disconnect", type: "radio", checked: true },
+    // { label: "Connect", type: "radio" },
+    { label: "Settings", icon: options.icon.settings },
     {
       label: "Exit",
       icon: options.icon.exit,
